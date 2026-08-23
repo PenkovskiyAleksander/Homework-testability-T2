c6f363a9b33b320b21082b3305fb94537477ddb0
Aleksander Penkovskiy <san-eck@mail.ru>
2026-08-23 19:51:55 +0300

    2

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        double height = 1.87;
-        double weight = 98;
+        double height = 1.69;
+        double weight = 76;
         int bmi = service.calculate(height, weight);
 
         System.out.println("Индекс массы тела: " + bmi);
